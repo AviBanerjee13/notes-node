7177ebc3cb0b47822883d0e59ffa4028d878ed88
Abhishek Banerjee <abhishekbanerjee4117@gmail.com>
2017-05-30 17:48:33 -0400

    Started work on node example program

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('Starting app.js ...');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+var user = os.userInfo();
+console.log(`Hello ${user.username}!`);
+
+//fs.appendFile('greetings.txt','Hello World!');
