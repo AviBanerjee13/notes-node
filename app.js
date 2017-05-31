58392cd09a4482acfd46e0ef640816c6e27ba196
Abhishek Banerjee <abhishekbanerjee4117@gmail.com>
2017-05-30 23:27:45 -0400

    package.json added, gitignore added, cleaned up app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 console.log('Starting app.js ...');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-console.log(`Hello ${user.username}!`);
+var command = process.argv[2];
+console.log('Command: ', command);
 
-//fs.appendFile('greetings.txt','Hello World!');
+if(command === 'add'){
+    console.log('Adding new note');
+} else if(command === 'list'){
+    console.log('Listing all notes');
+} else{
+    console.log('Command not recognized');
+}
