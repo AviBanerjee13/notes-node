4b5efc9a8b16c0f753ab3605a74c77fd2262e341
Abhishek Banerjee <abhishekbanerjee4117@gmail.com>
2017-06-16 00:28:50 -0400

    checking in unsaved files

--- notes.js
+++ notes.js
@@ -1,1 +1,9 @@
 console.log('Starting notes.js...');
+
+var addNote = (title, body) => {
+    console.log('Adding note: ', title, body);
+};
+
+module.exports = {
+    addNote //addNote: addNote, in ES6 exported obj is same name as variable, we can use just one name
+}
